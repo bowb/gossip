d32a18139347eb5510a60385a42a648b1ea8288d
bowb <6180319+bowb@users.noreply.github.com>
2024-04-05 17:03:51 -0700

    glog out of headers.

--- tests/test_swim_server.cpp
+++ tests/test_swim_server.cpp
@@ -1,7 +1,8 @@
 // Copyright (c) 2016 AlertAvert.com. All rights reserved.
 // Created by M. Massenzio (marco@alertavert.com) on 11/24/16.
-#include <glog/logging.h>
+
 #include <chrono>
+#include <glog/logging.h>
 #include <memory>
 #include <thread>
 
